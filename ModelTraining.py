8d5bd874ef9255f6bbbb521436339ff0a033cd58
Eslam Elhosuniy <119428009+eslamelhosuniy@users.noreply.github.com>
2025-11-22 13:35:54 +0200

    Train and save the RandomForest classifier
    - Loaded dataset from data.pickle and processed features/labels
    - Added label distribution logging (V / W / L counts)
    - Applied stratified train-test split to maintain class balance
    - Trained a RandomForestClassifier on the training set
    - Evaluated model accuracy on test set and printed results
    - Saved trained model to model.p using pickle

---
+++ ModelTraining.py
@@ -0,0 +1,39 @@
+import pickle
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import numpy as np
+
+
+data_dict = pickle.load(open('./data.pickle', 'rb'))
+data = np.asarray(data_dict['data'])
+print("length of data:", len(data))
+labels = np.asarray(data_dict['labels'])
+print("length of labels:", len(labels))
+v_count = w_count = l_count = 0
+for i in range(len(labels)):
+    if labels[i] == 0:
+        v_count+=1
+    elif labels[i] == 1:
+        w_count +=1
+    else:
+        l_count+=1
+print(f"v_count: {v_count}, w_count: {w_count}, l_count: {l_count}")
+
+data_train, data_test, labels_train, labels_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
+
+model = RandomForestClassifier()
+
+model.fit(data_train, labels_train)
+
+y_predict = model.predict(data_test)
+
+score = accuracy_score(y_predict, labels_test)
+
+print(f'{score * 100}% of samples were classified correctly !')
+
+f = open('model.p', 'wb')
+pickle.dump({'model': model}, f)
+f.close()
+
